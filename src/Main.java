1eb177ea44da07a8a88e802ed992b341c74def06
Valeriya Polukhina <valeriapolyxina@mail.ru>
2021-10-04 14:16:45 +0300

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 10_000;
+        int costPerMile = 20;
+        int totalMiles = ticketPrice / costPerMile;
+        System.out.println(totalMiles + " miles");
+    }
+}
